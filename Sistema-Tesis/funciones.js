98f315509d9ea327ad291c779a9027fae8840dbe
jcurvelo <juancurvelo99@gmail.com>
2021-03-18 16:51:07 -0430

    cambios

--- Sistema-Tesis/funciones.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('agregarCal').addEventListener('click',()=>{
-    window.location = 'http://localhost/Sistema-Tesis/home.php?cali=true';
-})
--- view-docente.php
+++ view-docente.php
@@ -1,23 +1,14 @@
-<div class="vista vista-docente container">
-    <button id="agregarCal" class="btn btn-primary mb-1 mt-2">Agregar Calificación</button>
-    <?php
-        if(isset($_GET['cali'])){
-            include './agregar-calificacion.php';
-        }
-    ?>
-    <table class="table">
-        <thead class="bg-dark">
-            <tr class="text-white">
-                <td>CI</td>
-                <td>Perido 1</td>
-                <td>Periodo 2</td>
-                <td>Periodo 3</td>
-                <td>Periodo 4</td>
-                <td>Total</td>
-            </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-    </table>
-</div>
+<div class="contenedor">
+        <h3 class="titulo">¿Que deseas hacer?</h3>
+        <div class="vista-docente container">
+            <button id="agregarCal" class="btn btn-primary mb-1 mt-2">Agregar Calificación</button>
+                 <?php
+                   if(isset($_GET['cali'])){
+                   include './agregar-calificacion.php';
+                   }
+                 ?>
+            </div>
+     <div class="vista-docente container">
+         <button id="cerrarSesion" class="btn btn-primary mb-1 mt-2">Cerrar Sesion</button>
+     </div>
+</div> 
